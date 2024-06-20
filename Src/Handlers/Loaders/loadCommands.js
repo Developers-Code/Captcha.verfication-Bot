afbccbadef7253fab06d27d3794e418a6c60a749
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2024-06-20 01:35:27 +0100

    Update loadCommands.js

--- Src/Handlers/Loaders/loadCommands.js
+++ Src/Handlers/Loaders/loadCommands.js
@@ -1,5 +1,16 @@
-function loadCommands(client, color) {
-    
-}
+async function loadCommands(client, color) {
+  const { readdirSync } = require("fs");
 
-module.exports = loadCommands;
+  // ========================================| Code |======================================= \\
+
+  const commandFolders = readdirSync(`${process.cwd()}/Src/Commands/Message`);
+  for (const folder of commandFolders) {
+    const commandFiles = readdirSync(`${process.cwd()}/Src/Commands/Message/${folder}/`).filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+      const command = require(`${process.cwd()}/Src/Commands/Message/${folder}/${file}`);
+      client.commands.set(command.name, command);
+    }
+    console.log(`${color.bold.green(`[MESSAGE COMMAND]`)} ` + `[${commandFiles.length}] `.cyan + `in `.yellow + `${folder} `.magenta + `was loaded!`.yellow);
+  }
+}
+module.exports= { loadCommands };
